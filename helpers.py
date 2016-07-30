7795b70bf29db02c59e079294b6cba447d77239c
Dylan Sather <dylan@dylburger.com>
2016-07-29 17:52:11 -0700

    Adding function to read geonames username from file

--- helpers.py
+++ helpers.py
@@ -3,6 +3,7 @@
 import yaml
 
 SOCRATA_APP_TOKEN_FILE = ".socrata_app_token"
+GEONAMES_USERNAME_FILE = ".geonames_username"
 
 
 def read_socrata_app_token(app_token_file=SOCRATA_APP_TOKEN_FILE):
@@ -24,3 +25,29 @@ def read_socrata_app_token(app_token_file=SOCRATA_APP_TOKEN_FILE):
 
         # If everything worked, return the app token
         return app_token_dict['token']
+
+
+def read_geonames_username(geonames_username_file=GEONAMES_USERNAME_FILE):
+    """ Accepts a YAML file containing the GeoNames username,
+        required for interacting with the API
+        Returns the username as a string for use in passing to
+        the GeoNames API
+
+        Sign up for a GeoNames account here:
+
+        http://www.geonames.org/login
+
+        Then enable access to the webservice here:
+
+        http://www.geonames.org/manageaccount
+    """
+    with open(geonames_username_file) as geonames_username_file_handle:
+        # Load app token from disk, handling basic exceptions
+        try:
+            geonames_dict = yaml.load(geonames_username_file_handle)
+        except yaml.YAMLError as exc:
+            print "Your username file doesn't appear to contain valid " \
+                   "YAML: ", exc
+
+        # If everything worked, return the username
+        return geonames_dict['username']
